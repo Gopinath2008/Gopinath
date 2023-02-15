6b0828f85c8a76840e171fa02f45080dc08db8e7
Gopinath2008 <122212515+Gopinath2008@users.noreply.github.com>
2023-02-15 09:06:14 +0530

    Update eligible to vote easy 6.c

--- eligible to vote easy 6.c
+++ eligible to vote easy 6.c
@@ -4,10 +4,15 @@ int main()
 	int age,a;
 	printf("enter the age:");
 	scanf("%d",&age);
-	a=18-age;
-	if(age>=18)
-	printf("you are eligible to vote now.");
-	else
-	printf("you are not eligible to vote now.\n");
-	printf("you have to wait %d years.\n",a);
+	if(age>0)
+	{
+		a=18-age;
+		if(age>=18)
+		printf("you are eligible to vote now.");
+		else
+		printf("you are not eligible to vote now.\n");
+		printf("you have to wait %d years.\n",a);
+	}
+	else 
+	printf("The given age is invalid");
 }
