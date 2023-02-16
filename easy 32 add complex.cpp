3c12b3ccf49104361533b4a6833143ecb11f2800
Gopinath2008 <122212515+Gopinath2008@users.noreply.github.com>
2023-02-16 10:05:36 +0530

    Add files via upload

---
+++ easy 32 add complex.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+typedef struct complex
+{
+	int real,imaginary;
+}complex;
+void addnumbers(complex x,complex y,complex *c);
+int main()
+{
+	struct complex x,y,c;
+	printf("Enter the value  first complex number:");
+	scanf("%d%d",&x.real,&x.imaginary);
+	printf("Enter the value of y of second complex number:");
+	scanf("%d%d",&y.real,&y.imaginary);
+	addnumbers(x,y,&c);
+	printf("Sum of complex numbers: %d + i%d",c.real,c.imaginary);
+	return 0;
+}
+void addnumbers(complex x,complex y,complex *c)
+{
+	c->real=x.real+y.real;
+	c->imaginary=x.imaginary+y.imaginary;
+}
